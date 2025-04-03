7883f18b31653d87f97306c40c81d462b498741a
FrancoAseglio <aseglio.f@gmail.com>
2025-04-03 19:34:13 +0200

    java dap integration
    requires .git at project dir
    lauches with'Launch...' (3rd option)

--- .config/nvim/lua/franco/plugins/dap.lua
+++ .config/nvim/lua/franco/plugins/dap.lua
@@ -7,15 +7,113 @@ return {
 
 		dap.configurations.cpp = dap.configurations.c
 
+		-- Java debug adapter configuration 81j
+		dap.configurations.java = {
+			{
+				type = "java",
+				request = "attach",
+				name = "Debug (Attach) - Remote",
+				hostName = "127.0.0.1",
+				port = 5005,
+			},
+			{
+				type = "java",
+				request = "launch",
+				name = "Debug (Launch) - Current File",
+				mainClass = "${file}", -- Ensures the main class is the current file
+				projectName = function()
+					return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+				end,
+				projectRoot = function()
+					return vim.fn.expand("%:p:h") -- This expands to the directory containing the current file.
+				end,
+			},
+		}
+
+		-- Setup integration between nvim-jdtls and nvim-dap
+		local jdtls_setup = function()
+			local mason_registry = require("mason-registry") -- Make sure java debug extension is present
+
+			-- Java Debug and Test dependencies
+			local java_debug_path = vim.fn.stdpath("data") .. "/mason/packages/java-debug-adapter"
+			local java_test_path = vim.fn.stdpath("data") .. "/mason/packages/java-test"
+
+			-- Ensure both packages are installed via Mason
+			if not mason_registry.is_installed("java-debug-adapter") then
+				vim.cmd("MasonInstall java-debug-adapter")
+			end
+
+			if not mason_registry.is_installed("java-test") then
+				vim.cmd("MasonInstall java-test")
+			end
+
+			local bundles = {
+				vim.fn.glob(java_debug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar", true),
+			}
+
+			-- Add Java test bundles
+			vim.list_extend(bundles, vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar", true), "\n"))
+
+			-- Set up Java LSP with debugging capability
+			local config = {
+				cmd = { "jdtls" },
+				root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+				settings = {
+					java = {
+						configuration = {
+							runtimes = {
+								{
+									name = "JavaSE-17",
+									path = "/Library/Java/JavaVirtualMachines/jdk-24.jdk/Contents/Home",
+								},
+							},
+						},
+					},
+				},
+				init_options = {
+					bundles = bundles,
+				},
+			}
+
+			require("jdtls").setup_dap({ hotcodereplace = "auto" }) -- Set up JAR files for debugging
+
+			return config
+		end
+
+		-- Set up jdtls on java file open
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "java",
+			callback = function()
+				local config = jdtls_setup()
+				require("jdtls").start_or_attach(config)
+			end,
+		})
+		---------- java debug setting 81k ----------
+
+		-- DAP keymaps
+		key("n", "<leader>ds", dap.continue, { desc = "Start aka Continue" })
+
+		key("n", "<leader><Right>", require("dap").continue, { desc = "Continue" })
+		key("n", "<leader><Up>", require("dap").step_over, { desc = "Step Over" })
+		key("n", "<leader><Down>", require("dap").step_into, { desc = "Step Into" })
+		key("n", "<leader><Left>", require("dap").step_out, { desc = "Step Out" })
+
 		key("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
-		key("n", "<leader>dc", dap.continue, { desc = "Continue" })
-		key("n", "<leader>do", dap.step_over, { desc = "Step Over" })
-		key("n", "<leader>di", dap.step_into, { desc = "Step Into" })
-		key("n", "<leader>dO", dap.step_out, { desc = "Step Out" })
-		key("n", "<leader>dt", dap.terminate, { desc = "Terminate" })
-		key("n", "<leader>dB", function()
+
+		key("n", "<leader>dc", function()
 			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
 		end, { desc = "Conditional Breakpoint" })
+
+		key("n", "<leader>dq", function()
+			require("dap").terminate()
+			require("dapui").close()
+			for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+				local name = vim.api.nvim_buf_get_name(buf)
+				if name:match("DAP") or name:match("REPL") then
+					vim.api.nvim_buf_delete(buf, { force = true })
+				end
+			end
+		end, { desc = "Quit" })
 
 		-- DAP UI setup
 		dapui.setup({
@@ -48,6 +146,7 @@ return {
 			},
 			windows = { indent = 1 },
 		})
+
 		dap.listeners.after.event_initialized["dapui_config"] = function()
 			dapui.open()
 		end
@@ -74,11 +173,12 @@ return {
 			virt_text_win_col = nil,
 		})
 
+		-- Mason DAP languages
 		require("mason-nvim-dap").setup({
 			automatic_setup = true,
 			automatic_installation = true,
 			handlers = {},
-			ensure_installed = { "codelldb" },
+			ensure_installed = { "codelldb", "java-debug-adapter" },
 		})
 	end,
 }
