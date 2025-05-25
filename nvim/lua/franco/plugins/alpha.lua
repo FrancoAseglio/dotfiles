47dbcfb8b077f0123a0c5a4820c5a59a44e6af95
FrancoAseglio <aseglio.f@gmail.com>
2025-05-25 21:33:16 +0200

    extra
    introduced auto-session.lua
    alpha.lua unused buttons substitution

--- nvim/lua/franco/plugins/alpha.lua
+++ nvim/lua/franco/plugins/alpha.lua
@@ -64,13 +64,12 @@ return {
 
 		-- Set menu with direct command execution
 		dashboard.section.buttons.val = {
+			-- create_button("SPC + rs", "  > Session Restore", ""),
 			create_button("SPC + fe", "  > File Explorer", ""),
 			create_button("SPC + ff", "󰱼  > Fuzzy File", ""),
 			create_button("SPC + fs", "  > Live Grep", ""),
 			create_button("SPC + lg", "  > LazyGit", ""),
 			create_button("SPC + gc", "  > Config", ""),
-			-- create_button("SPC + mm", "󱁤  > Mason", ""),
-			-- create_button("SPC + dd", "󰆼  > DBUI", ""),
 			create_button("SPC + la", "󰒲  > Lazy", ""),
 		}
 		dashboard.section.buttons.opts.hl = "AlphaButtons"
