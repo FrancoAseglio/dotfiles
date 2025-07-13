b5b2bdb7867ff8da41e0d4c86e5e445bd33107d2
FrancoAseglio <aseglio.f@gmail.com>
2025-07-13 14:26:01 +0200

    minor
    indent-blankline integrations

--- nvim/lua/franco/plugins/indent-blankline.lua
+++ nvim/lua/franco/plugins/indent-blankline.lua
@@ -1,8 +1,51 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
-  opts = {
-    indent = { char = "┊" },
-  },
+	"lukas-reineke/indent-blankline.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	main = "ibl",
+	opts = {
+		indent = {
+			char = "┊",
+		},
+
+		scope = {
+			enabled = true,
+			char = "┊",
+			show_start = false,
+			show_end = false,
+			include = {
+				node_type = {
+					["*"] = {
+						-- Control structures
+						"if_statement",
+						"for_statement",
+						"while_statement",
+						"try_statement",
+						"with_statement",
+						"match_statement",
+						-- Functions and classes
+						"class_definition",
+						"function_definition",
+						"method_definition",
+					},
+				},
+			},
+		},
+
+		exclude = {
+			filetypes = {
+				"help",
+				"man",
+				"lspinfo",
+				"checkhealth",
+				"TelescopePrompt",
+				"TelescopeResults",
+				"qf",
+			},
+			buftypes = {
+				"terminal",
+				"nofile",
+				"prompt",
+			},
+		},
+	},
 }
