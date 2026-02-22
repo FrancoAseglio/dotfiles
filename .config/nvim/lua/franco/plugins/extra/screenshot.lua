720e6220928d3f4a4a0d6ffbca9101956c86e598
FrancoAseglio <aseglio.f@gmail.com>
2026-02-22 11:10:51 +0100

    refactor
    pinned CodeSnap old version(waiting for new version migration)

--- .config/nvim/lua/franco/plugins/extra/screenshot.lua
+++ .config/nvim/lua/franco/plugins/extra/screenshot.lua
@@ -1,14 +1,20 @@
 return {
 	"mistricky/codesnap.nvim",
 	build = "make",
+	version = "^1", -- pinned to old stable version
+
+	config = function(_, opts)
+		require("codesnap").setup(opts)
+    vim.keymap.set("x", "<leader>cs", ":CodeSnapSave<cr>", { noremap = true, silent = true })
+	end,
+
 	opts = {
-		mac_window_bar = false,
-		save_path = "~/CodeSnap/",
-		has_breadcrumbs = false,
-		bg_theme = "bamboo",
+		mac_window_bar  = false,
 		has_line_number = true,
-		title = "",
-		watermark = "",
+		has_breadcrumbs = false,
+		save_path       = "~/Screenshots/",
+		watermark       = "",
+		title           = "",
+		bg_theme        = "default",
 	},
-	vim.keymap.set("v", "<leader>cs", ":CodeSnapSave<CR>", { noremap = true, silent = true }),
 }
